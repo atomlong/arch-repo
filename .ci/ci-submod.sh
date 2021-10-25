970ba0aad0735653726ff73605da7cf433c4ecc5
atomlong <atom.long@hotmail.com>
2021-10-25 21:52:27 +0800

    support private sub repo.

--- .ci/ci-submod.sh
+++ .ci/ci-submod.sh
@@ -3,14 +3,15 @@
 # Check if a repo exist
 check_repo_exist()
 {
-local repo_name=${1}
-local repo_url=$(sed -r "s|(.*/)[^/]+(\s*$)|\1${repo_name##*/}.git\2|" <<< ${GH_URL})
+local repo_name="${1}"
+local repo_url=$(sed -r "s|^([^/]+//)([^/]+/.*/)[^/]+(\s*)$|\1${GH_USER}:${GH_TOKEN}@\2${repo_name##*/}.git\3|" <<< "${GH_URL}")
 local RES
 while true; do
-RES=$(curl -L -o /dev/null -s -w "%{http_code}" ${repo_url})
-[ "$?" == 0 ] || continue
-[ "${RES}" == 200 ] && { echo "${repo_url}"; return 0; }
-[ "${RES}" == 404 ] && return 1
+RES=$(GIT_TERMINAL_PROMPT=0 git ls-remote ${repo_url} 2>&1)
+[ $? == 0 ] && { echo "${repo_url}"; return 0; }
+grep -q "^fatal: unable to access" <<< "${RES}" && continue
+grep -q "^error: RPC failed" <<< "${RES}" && continue
+return 1
 done
 }
 
@@ -26,11 +27,13 @@ echo "Submodule ${REPO_NAME} exists."
 return 0
 }
 
-REPO_URL=$(check_repo_exist ${REPO_NAME}) || { echo "Not found repo ${REPO_NAME} on your account."; return 1; }
+REPO_URL=$(check_repo_exist ${REPO_NAME}) || { echo "Not found repo ${REPO_NAME} on your account."; set +x; return 1; }
 
 while ! git submodule add "${REPO_URL}" "${REPO_NAME}" 2>/dev/null; do
 rm -rf "${REPO_NAME}"
 done
+REPO_URL=$(printf "${REPO_URL}" | sed -r 's#(.*//)[^/]*@(.*)$#\1\2#')
+git submodule set-url "${REPO_NAME}" "${REPO_URL}"
 
 submodules=($(sed -n -r 's/^\[submodule\s+"(.*)"\s*\]\s*$/\1/p' .gitmodules | sort -u))
 mod_insert=$(sed -r "s/(^|.*\s)${REPO_NAME}( (\S+).*|$)/\3/" <<< "${submodules[@]}")
@@ -69,6 +72,27 @@ rm -rf .git/modules/${REPO_PATH}
 git commit -m "remove submodule \"${REPO_NAME}\""
 }
 
+# update submodules
+submodule_sync()
+{
+[ "$#" == 1 ] || { echo "Usage: submodule_sync submod"; return 1; }
+local REPO_NAME=${1}
+expect << EOF
+set timeout 300
+spawn git submodule update --progress --remote "${REPO_NAME}"
+expect "Username for"
+send "${GH_USER}\r"
+expect "Password for"
+send "${GH_TOKEN}\r"
+expect EOF
+EOF
+
+git diff --exit-code "${REPO_NAME}" || {
+git add "${REPO_NAME}"
+git commit -m "Submodule Update" -a
+}
+}
+
 # Add/Remove a submodule as build marker file
 submodule_auto()
 {
@@ -80,6 +104,10 @@ https://efiles.cf/archlinuxarm/armv7h/build.marker
 local bm mod submodules
 
 submodules=($(git config --file .gitmodules --name-only --get-regexp "submodule\..*\.path" | grep -Po '^submodule\.\K\S+(?=\.path)'))
+
+for mod in ${submodules}; do
+submodule_sync ${mod}
+done
 
 for bm in ${BM_FILES[@]}; do
 for mod in $(curl -sL ${bm} | grep -Po '^\[\w+\]((?!://).)*?\K[^/\s]+(?=\s*$)'); do
@@ -96,6 +124,8 @@ done
 THIS_DIR=$(readlink -f "$(dirname ${0})")
 ROOT_DIR=$(readlink -f ${THIS_DIR}/../)
 GH_URL=$(git config --get remote.origin.url)
+GH_USER=$(grep -Po '\.\w+/\K[^/]+(?=/)' <<< "${GH_URL}")
+[ -z "${GH_TOKEN}" ] && { echo "GH_TOKEN not set"; exit 1; }
 COMMAND=${1}
 shift
 REPO_LIST=(${@})
